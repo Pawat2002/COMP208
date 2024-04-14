8f60ab25704b35eb16fa1b0ce8e146a4e85d5e9e
yung1818 <120514146+yung1818@users.noreply.github.com>
2024-04-14 18:23:31 +0100

    Add files via upload

--- fbwg.py
+++ fbwg.py
@@ -6,29 +6,50 @@ pygame.init()
 clock = pygame.time.Clock()
 fps = 60
 
-screen_width = 1000
-screen_height = 1000
+screen_width = 800
+screen_height = 800
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Fire boy and Water girl")
 
 # define game tile size
-tile_size = 50
+tile_size = 40
+game_over = 0
+main_menu = True
+
 
 # load imgs
-sky_img = pygame.image.load('sky.jpg')
+bg = pygame.image.load('bg.png')
+reset = pygame.image.load('gameover.png')
+start = pygame.image.load('start.png')
+quit = pygame.image.load("quit.png")
 
-# sound effects
-jumpSound = pygame.mixer.Sound("jump.wav")
 
-def bg_music():
-    if not pygame.mixer.music.get_busy():
-        # Load audio file
-        mixer.music.load("arcade_music.mp3")
-        #play the music
-        mixer.music.play(-1)
-        #set bg_music volume
-        pygame.mixer.music.set_volume(0.3)
+
+class Button():
+    def __init__(self,x,y,image):
+        self.image = image
+        self.image = pygame.transform.scale(self.image, (120, 80))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.clicked = False
+    def draw(self):
+        action = False
+
+        screen.blit(self.image,self.rect)
+        # mouse position
+        pos = pygame.mouse.get_pos()
+
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                action = True
+                self.clicked = True
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+        return action
+
+
 
 
 def draw_grid():
@@ -40,7 +61,7 @@ def draw_grid():
 # create player
 class Player():
     def __init__(self, x, y):
-        img = pygame.image.load('people.jpg')
+        img = pygame.image.load('people.png')
         self.image = pygame.transform.scale(img, (40, 80))
         # obtain position of people
         self.rect = self.image.get_rect()
@@ -50,69 +71,82 @@ class Player():
         self.height = self.image.get_height()
         self.vel_y = 0
         self.jumped = False
+        self.lives = 3
 
     # movement of people
-    def update(self):
+    def update(self,game_over):
         dx = 0
         dy = 0
 
-        # get keypresses
-        key = pygame.key.get_pressed()
-        if key[pygame.K_SPACE] and self.jumped == False:
-            jumpSound.play()
-            self.vel_y = -15
-            self.jumped = True
-        if key[pygame.K_SPACE] == False:
-            self.jumped = False
-        if key[pygame.K_LEFT]:
-            dx -= 5
-        if key[pygame.K_RIGHT]:
-            dx += 5
+        if game_over == 0:
+            # get keypresses
+            key = pygame.key.get_pressed()
+            if key[pygame.K_SPACE] and self.jumped == False:
+                self.vel_y = -15
+                self.jumped = True
+            if key[pygame.K_SPACE] == False:
+                self.jumped = False
+            if key[pygame.K_LEFT]:
+                dx -= 5
+            if key[pygame.K_RIGHT]:
+                dx += 5
 
-        # add gravity
-        self.vel_y += 1
-        if self.vel_y > 3:
-            self.vel_y = 3
+            # add gravity
+            self.vel_y += 1
+            if self.vel_y > 3:
+                self.vel_y = 3
 
-        dy += self.vel_y
+            dy += self.vel_y
 
-        # check collision
-        for tile in world.tile_list:
-            # x direction
-            if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
-                # check if below the ground eg jumping
-                dx = 0
+            # check collision
+            for tile in world.tile_list:
+                # x direction
+                if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+                    # check if below the ground eg jumping
+                    dx = 0
 
-            # y direction
-            if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
-                # check if below the ground eg jumping
-                if self.vel_y < 0:
-                    dy = tile[1].bottom - self.rect.top
-                    self.vel_y = 0
+                # y direction
+                if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+                    # check if below the ground eg jumping
+                    if self.vel_y < 0:
+                        dy = tile[1].bottom - self.rect.top
+                        self.vel_y = 0
 
-                # falling
-                elif self.vel_y >= 0:
-                    dy = tile[1].top - self.rect.bottom
-                    self.vel_y = 0
+                    # falling
+                    elif self.vel_y >= 0:
+                        dy = tile[1].top - self.rect.bottom
+                        self.vel_y = 0
 
-        # update player coordinates
-        self.rect.x += dx
-        self.rect.y += dy
+            # check collision with enemies
+            if pygame.sprite.spritecollide(self,enemy_group , False):
+                game_over = -1
+                self.lives -= 1  # Decrease lives when colliding with enemies
 
-        if self.rect.bottom > screen_height - 50:
-            self.rect.bottom = screen_height - 50
-            dy = 0
+
+
+
+            # update player coordinates
+            self.rect.x += dx
+            self.rect.y += dy
+
+        elif game_over == -1:
+            self.rect.x = 100
+            self.rect.y = screen_height - 130
+            game_over = 0
+            if self.lives == 0:
+                game_over = -1
+                reset_button.draw()
 
         # draw player onto screen
         screen.blit(self.image, self.rect)
         pygame.draw.rect(screen, (255, 255, 255), self.rect, 2)
-
+        return(game_over)
 
 class World():
     def __init__(self, data):
         self.tile_list = []
         dirt_img = pygame.image.load('brick.png')
-        grass_img = pygame.image.load('grass.webp')
+        coin_img = pygame.image.load('coin.png')
         block_img = pygame.image.load('block.png')
         door_img = pygame.image.load('door.png')
         row_count = 0
@@ -126,13 +160,10 @@ class World():
                     img_rect.y = tile_size * row_count
                     tile = (img, img_rect)
                     self.tile_list.append(tile)
-                if tile == 2:  # tile_list[1] grass
-                    img = pygame.transform.scale(grass_img, (tile_size, tile_size))
-                    img_rect = img.get_rect()
-                    img_rect.x = tile_size * col_count
-                    img_rect.y = tile_size * row_count
-                    tile = (img, img_rect)
-                    self.tile_list.append(tile)
+                if tile == 2:  # tile_list[0] dirt
+                    coin = Coin(col_count * tile_size, row_count * tile_size)
+                    coin_group.add(coin)
+
                 if tile == 3:  # tile_list[2] block
                     img = pygame.transform.scale(block_img, (tile_size, tile_size))
                     img_rect = img.get_rect()
@@ -161,8 +192,8 @@ class World():
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("enemy.jpg")
-        self.image = pygame.transform.scale(self.image, (50, 50))
+        self.image = pygame.image.load("enemy.png")
+        self.image = pygame.transform.scale(self.image, (40, 40))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -172,7 +203,17 @@ class Door(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("door.png")
-        self.image = pygame.transform.scale(self.image, (50, 50))
+        self.image = pygame.transform.scale(self.image, (40, 40))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+
+class Coin(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("coin.png")
+        self.image = pygame.transform.scale(self.image, (30, 30))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -182,23 +223,23 @@ class Door(pygame.sprite.Sprite):
 world_data = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 5, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 3, 0, 0, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 4, 0, 0, 2, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 0, 0, 3, 1],
+    [1, 0, 0, 0, 0, 3, 3, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 3, 3, 0, 0, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 2, 4, 1],
+    [1, 0, 0, 0, 0, 3, 3, 3, 3, 3, 0, 0, 0, 0, 3, 0, 0, 3, 3, 1],
+    [1, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 4, 0, 0, 1],
     [1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1],
 ]
 
@@ -208,26 +249,43 @@ enemy_group = pygame.sprite.Group()
 
 door_group = pygame.sprite.Group()
 
+coin_group = pygame.sprite.Group()
+
+
+
 world = World(world_data)
+
+reset_button = Button(screen_width//2 - 75, screen_height//2,reset)
 # if save_button.draw():
 # pickle_out(world_data,pickle_out)
+start_button = Button(screen_width//2-150, screen_height//2-40,start)
+quit_button = Button(screen_width//2+75, screen_height//2-40,quit)
+
+
 
 run = True
 while run:
 
-    bg_music()
-    
     clock.tick(fps)
-    screen.blit(sky_img, (0, 0))
-    world.draw()
+    screen.blit(bg, (0, 0))
+    if main_menu == True:
+        if start_button.draw():
+            main_menu = False
+        if quit_button.draw():
+            run = False
+    else:
+        world.draw()
 
-    draw_grid()
+        # draw_grid()
 
-    enemy_group.draw(screen)
+        enemy_group.draw(screen)
 
-    door_group.draw(screen)
+        door_group.draw(screen)
+        coin_group.draw(screen)
 
-    player.update()
+        game_over = player.update(game_over)
+
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
